ec71a3c6f2fa163d835fbf32da66e80b319e91f2
zainab-gilani <zainab.gilani@yahoo.com>
2025-06-29 22:29:32 +0400

    Created login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
@@ -23,5 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact")
+    path('contact', views.contact, name="contact"),
+
+    # Django Auth
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="accounts/login.html/"), name='login')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 
 def index(request):
+    print(request.user)
     return render(request, 'index.html')
 #enddef
 
