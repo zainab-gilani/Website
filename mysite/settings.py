25b2635f9360ce2f6d0bfe5d29adce56c839c4f4
zainab-gilani <zainab.gilani@yahoo.com>
2025-06-29 22:47:31 +0400

    Created logout feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
--- mysite/urls.py
+++ mysite/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
 
     # Django Auth
-    path('accounts/login/', auth_views.LoginView.as_view(template_name="accounts/login.html/"), name='login')
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="accounts/login.html/"), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name="logout")
 ]
