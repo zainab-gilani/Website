b83832a426e74f210cced3900fb80b6a0d525a76
zainab-gilani <zainab.gilani@yahoo.com>
2025-06-30 15:05:44 +0400

    Created user profile

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     print(request.user)
@@ -14,3 +14,6 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 #enddef
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
