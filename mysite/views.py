52ecfdde9ac69680cfa901facb0077f5f99108b3
zainab-gilani <zainab.gilani@yahoo.com>
2025-06-30 15:18:26 +0400

    Reformatted files

--- mysite/views.py
+++ mysite/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-def index(request):
+
+def index(request)-> HttpResponse:
     print(request.user)
-    return render(request, 'index.html')
-#enddef
+    return render(request, "index.html")
+# enddef
 
-def about(request):
-    return render(request, 'about.html')
-#enddef
 
-def contact(request):
-    return render(request, 'contact.html')
-#enddef
+def about(request)-> HttpResponse:
+    return render(request, "about.html")
+# enddef
+
+
+def contact(request)-> HttpResponse:
+    return render(request, "contact.html")
+# enddef
+
 
 class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/profile.html'
+    template_name = "accounts/profile.html"
+#endclass
