73edd2dd06eca335f66989411c59fc1f3e2da74c
zainab-gilani <zainab.gilani@yahoo.com>
2025-07-12 17:32:45 +0400

    Used CSS for sign up page

--- mysite/apps/accounts/forms.py
+++ mysite/apps/accounts/forms.py
@@ -3,22 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': '', 'class': 'form-input'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '', 'class': 'form-input'})
+    )
     password1 = forms.CharField(
         label='Password',
         strip=False,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+        widget=forms.PasswordInput(attrs={'placeholder': '', 'class': 'form-input'})
     )
-
     password2 = forms.CharField(
-        label='Password confirmation',
+        label='Confirm Password',
         strip=False,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'})
+        widget=forms.PasswordInput(attrs={'placeholder': '', 'class': 'form-input'})
     )
-#endclass
 
-class Meta:
-    model = User
-    fields = ('username', 'email', 'password1', 'password2')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+    #endclass
 #endclass
