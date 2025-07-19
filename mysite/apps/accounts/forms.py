8dd36452fd31094463e3157954c38b1c6e29af5b
zainab-gilani <zainab.gilani@yahoo.com>
2025-07-19 17:42:18 +0400

    Made login and added logo at the top left of the web pages

--- mysite/apps/accounts/forms.py
+++ mysite/apps/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
@@ -33,4 +34,19 @@ class CustomUserCreationForm(UserCreationForm):
         #endif
         return email
     #enddef
+#endclass
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': ''
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': ''
+        })
+    )
 #endclass
--- mysite/apps/accounts/urls.py
+++ mysite/apps/accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 from .views import resend_activation_view
+from .forms import CustomLoginForm
 
 app_name = "accounts"
 
@@ -12,7 +13,10 @@ urlpatterns = [
     # Django Auth
     path(
         "login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        auth_views.LoginView.as_view(
+            template_name="accounts/login.html",
+            authentication_form=CustomLoginForm
+        ),
         name="login",
     ),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
