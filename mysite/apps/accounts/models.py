b4763d3574723ed48d20ee621f1749c88a4d2f60
zainab-gilani <zainab.gilani@yahoo.com>
2025-08-24 18:33:05 +0400

    Removed old user models, added saved_match model

--- mysite/apps/accounts/models.py
+++ mysite/apps/accounts/models.py
@@ -1,40 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.http import request
 
-
-class UserInterest(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    normalized_name = models.CharField(max_length=64, unique=True)
-
-    def __str__(self):
-        return self.name
-    # enddef
-# endclass
-
-
-class UserPersona(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    normalized_name = models.CharField(max_length=64, unique=True)
-    description = models.CharField(max_length=200)
+class SavedMatch(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="saved_matches")
+    university = models.CharField(max_length=200)
+    course = models.CharField(max_length=200)
+    type = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
+    requirements = models.CharField(max_length=300)
+    course_link = models.URLField()
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.university} ({self.course})"
     #enddef
 #endclass
-
-
-# # Create your models here.
-# class UserProfile(models.Model):
-#     # Owner
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-#
-#     # Settings
-#     is_full_name_displayed = models.BooleanField(default=True)
-#
-#     # Details
-#     bio = models.CharField(max_length=500, blank=True, null=True)
-#     website = models.URLField(max_length=200, blank=True, null=True)
-#     persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
-#     interests = models.ManyToManyField(UserInterest, blank=True)
-# #endclass
