071e443ed8beddd976a0af407e503b60c851469c
zainab-gilani <zainab.gilani@yahoo.com>
2025-10-07 08:34:24 +0400

    Changed variable name, added .is_saved

--- mysite/apps/coursefinder/types.py
+++ mysite/apps/coursefinder/types.py
@@ -1,10 +1,11 @@
 class UniMatchResult:
-    def __init__(self, university, course, type, duration, requirements, course_link):
+    def __init__(self, university, course, course_type, duration, requirements, course_link, is_saved=False):
         self.university = university
         self.course = course
-        self.type = type
+        self.course_type = course_type
         self.duration = duration
         self.requirements = requirements
         self.course_link = course_link
+        self.is_saved = is_saved
     #enddef
 #endclass
