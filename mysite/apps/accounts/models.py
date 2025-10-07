f8919e51be32932796938e6fccf25e02b3be744f
zainab-gilani <zainab.gilani@yahoo.com>
2025-10-07 08:26:10 +0400

    Changed variable names

--- mysite/apps/accounts/models.py
+++ mysite/apps/accounts/models.py
@@ -5,10 +5,10 @@ class SavedMatch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="saved_matches")
     university = models.CharField(max_length=200)
     course = models.CharField(max_length=200)
-    type = models.CharField(max_length=100)
+    course_type = models.CharField(max_length=100)
     duration = models.CharField(max_length=100)
     requirements = models.CharField(max_length=300)
-    course_link = models.URLField()
+    course_link = models.URLField(max_length=500)
 
     def __str__(self):
         return f"{self.user.username} - {self.university} ({self.course})"
